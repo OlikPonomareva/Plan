d1f82b8d379aa620962025fddc319ed6e2275e7f
Olga Ponomareva <olga.ponomareva5@gmail.com>
2019-07-02 11:11:54 +0300

    Пустой проект

---
+++ IU.Planning.ConApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IU.Planning.ConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
